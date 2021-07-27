bf08a3162ff9d10a460497b3433f80ae600513ce
Litnine <savchenko2002je@gmail.com>
2021-07-27 22:54:57 +0300

    some small changes

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controllers;
 
 import com.example.demo.pojos.drawables.Curve;
+import com.example.demo.pojos.drawables.Note;
+import com.example.demo.pojos.drawables.Text;
 import com.example.demo.pojos.drawables.utils.Drawable;
 import com.example.demo.pojos.drawables.utils.Point;
 import com.example.demo.utils.Sender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -17,14 +20,31 @@ public class MainController {
     }
 
     @GetMapping("/getData")
-    public String getCanvas(Map<String, Object> model) {
-        System.out.println(Sender.toJSON(
+    public @ResponseBody
+    String getCanvas(Map<String, Object> model) {
+        String body = "";
+        body += Sender.toJSON(
+                new Text(
+                        new Drawable(new Point(0, 0)),
+                        "I'm a text element"
+                ));
+        body += "<br>";
+        body += Sender.toJSON(
+                new Note(
+                        new Drawable(new Point(0, 0)),
+                        "I'm a note element",
+                        2,
+                        5
+                ));
+        body += "<br>";
+        body += Sender.toJSON(
                 new Curve(
                         new Drawable(new Point(0, 0)),
                         new Point[]{new Point(1, 1)},
                         "#000",
                         5
-                )));
-        return "index";
+                ));
+        body += "<br>";
+        return body;
     }
 }
--- src/main/java/com/example/demo/utils/Sender.java
+++ src/main/java/com/example/demo/utils/Sender.java
@@ -3,35 +3,22 @@ package com.example.demo.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Sender {
-    private static final ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+    private static final ObjectWriter ow = includeClassNameInObjectMapper(new ObjectMapper())
+            .writer().withDefaultPrettyPrinter();
 
-    private static class Sendable {
-        private final Object object;
-        private final String type;
-
-        public Sendable(Object object) {
-            this.object = object;
-            this.type = object.getClass().getSimpleName();
-        }
-
-        public Object getObject() {
-            return object;
-        }
-
-        public Object getType() {
-            return type;
-        }
+    private static ObjectMapper includeClassNameInObjectMapper(ObjectMapper om){
+        om.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        return om;
     }
 
     public static String toJSON(Object obj){
         try {
-            return ow.writeValueAsString(
-                    new Sendable(obj)
-            );
+            return ow.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
             return "{}";
         }
